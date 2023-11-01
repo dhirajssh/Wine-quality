85559728f5145a1f7f864036eaa01ee3b023c15a
Dhiraj Shah <dhirajshah@Dhirajs-MacBook-Air.local>
2023-11-01 17:09:42 +0530

    CORS error fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model.model import predict_pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 class TextIn(BaseModel):
   fixed_acidity: float
